da26cc0eb9ed1223c3643a6860aee7fb49481fd6
Gabriel <gabysolotorevsky100@gmail.com>
2024-11-10 20:37:09 -0300

    workers

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ import json
 import ssl
 
 def get_ssl_context(certfile, keyfile):
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
     context.load_cert_chain(certfile, keyfile)
     context.set_ciphers("@SECLEVEL=1:ALL")
     return context
